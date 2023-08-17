ae0387a7365c38b8095fb45f35296f6485b48560
Raju1310 <Rajuespi13@gmail.com>
2023-08-17 12:22:10 +0530

    number_management

---
+++ number management/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 8008;
+
+app.get('/numbers', async (req, res) => {
+  const requestedUrls = req.query.url || [];
+  const collectedNumbers = [];
+
+  try {
+    await Promise.all(
+      requestedUrls.map(async (url) => {
+        try {
+          const response = await axios.get(url);
+          if (response.data.numbers) {
+            collectedNumbers.push(...response.data.numbers);
+          }
+        } catch (error) {
+          console.error(`Error fetching data from ${url}:`, error.message);
+        }
+      })
+    );
+
+    const uniqueNumbers = Array.from(new Set(collectedNumbers));
+    const sortedNumbers = uniqueNumbers.sort((a, b) => a - b);
+
+    res.json({ numbers: sortedNumbers });
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
